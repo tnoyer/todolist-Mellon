915bcd6de73ecdca5dfb671c4fce58431ce9dbbe
Théophile <theophile.noyer@hotmail.fr>
2019-02-28 10:53:07 +0100

    method addTextChangedListener

--- app/src/main/java/com/noyer/theo/todolist/MainActivity.kt
+++ app/src/main/java/com/noyer/theo/todolist/MainActivity.kt
@@ -3,26 +3,50 @@ package com.noyer.theo.todolist.controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 import com.noyer.theo.todolist.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
 
-    // Initializing an empty ArrayList to be filled with tasks
+    //Initialisation d'une ArrayList vide à remplir avec des tâches
     val tasks: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var mValidButton = findViewById<Button>(R.id.activity_main_valid_btn)
+        //initialisation des variables
+        var mTitleForm = findViewById<TextView>(R.id.activity_main_title_form)
         var mTaskInput = findViewById<EditText>(R.id.activity_main_task_input)
+        var mValidButton = findViewById<Button>(R.id.activity_main_valid_btn)
+        var mTitleList = findViewById<TextView>(R.id.activity_main_title_list)
+        var mDeleteButton = findViewById<Button>(R.id.list_item_delete_btn)
 
         mValidButton.isEnabled = false
 
-        // Loads tasks into the ArrayList
+        //fonction qui s'applique lors du changement de texte dans la variable mTaskInput
+        mTaskInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            //Bouton inactif si pas de texte
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                mValidButton.isEnabled = s.toString().isNotEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+        // appel de la fonction pour afficher la liste des taches
         addTasks()
 
         // Creates a vertical Layout Manager
